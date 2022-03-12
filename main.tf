36a496a3066560b962b69df08e3dc16273382904
pankajsahara <46815838+pankajsahara@users.noreply.github.com>
2022-03-12 14:18:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "Av+rl+rKgmoGoUoLHErUbwuL4yLtJOEfnHZL+PVH"
 }
 
-resource "aws_instance" "webserver" {
+resource "aws_instance" "webserver": {
   ami           = "ami-06a0b4e3b7eb7a300" # ap-south-1
   instance_type = "t3.medium"
   
